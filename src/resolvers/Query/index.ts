9b6b51bbd41eac5be9243c0841a0d493d104c8cc
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-03-20 21:46:05 +0100

    chore(setup): setup graphql-yoga and typescript
    - setup prettier
    - setup resolvers
    
    [Maintains #164761442]

---
+++ src/resolvers/Query/index.ts
@@ -0,0 +1,4 @@
+export default {
+  info: () =>
+    'Population Management System is an API that contains a list of locations and the total number of residents in each location broken down by gender',
+};
