cde23ff1bc66612e14ba82e380c0e92f222c4402
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-03-24 00:23:25 +0100

    chore(circleci): setup continuous integration
    - setup test prisma service
    - install testing tools
    - write a single test
    - setup circleci
    - add new env variables
    
    [Maintains #164825956]

---
+++ src/__mocks__/prisma.ts
@@ -0,0 +1,15 @@
+import { makePrismaClientClass } from 'prisma-client-lib';
+import {
+  ClientConstructor,
+  Prisma,
+  models,
+} from '../prisma/generated/prisma-client';
+import { typeDefs } from '../prisma/generated/prisma-client/prisma-schema';
+
+const prismaTestMockClient = makePrismaClientClass<ClientConstructor<Prisma>>({
+  typeDefs,
+  models,
+  endpoint: `${process.env['PRISMA_API_URL_TEST']}`,
+});
+
+export default prismaTestMockClient;
